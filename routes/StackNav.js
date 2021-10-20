75bfa0bf80c76685ec93cac9d09a343e9adf902d
Shompoth KHAN <dadoulan@gmail.com>
2021-10-20 16:47:33 +0200

    removed header on Home screen

--- routes/StackNav.js
+++ routes/StackNav.js
@@ -11,7 +11,11 @@ const Stack = createStackNavigator();
 const StackNav = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Accueil" component={Home} />
+            <Stack.Screen
+                name="Accueil"
+                component={Home}
+                options={{ headerShown: false }}
+            />
             <Stack.Screen name="Posts" component={Posts} />
         </Stack.Navigator>
     );
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Pressable } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Colors from "../styles/Colors";
 
 const Home = ({ navigation }) => {
     // Fonction
@@ -10,20 +11,40 @@ const Home = ({ navigation }) => {
         <View style={styles.container}>
             <Text>Home</Text>
             {/* <Button title="Accéder aux posts" onPress={handlePress} /> */}
-            <Pressable
+            {/* <Pressable
                 style={({ pressed }) => ({
                     backgroundColor: pressed ? "lightseagreen" : "rebeccapurple",
                 })}
                 onPress={handlePress}
             >
                 <Text style={styles.btn}>Accéder aux Posts</Text>
-            </Pressable>
+            </Pressable> */}
+            <TouchableOpacity
+                style={styles.btn}
+                onPress={handlePress}
+                activeOpacity={0.3}
+            >
+                <Text style={styles.btnText}>Accéder aux Posts</Text>
+            </TouchableOpacity>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: "center", alignItems: "center" },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.secondary,
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 9,
+        borderRadius: 5,
+    },
+    btnText: {
+        color: Colors.white,
+    },
 });
 
 export default Home;
