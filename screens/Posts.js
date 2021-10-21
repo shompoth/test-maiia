373c887e82ec63be23650fb14b96756a6e0cd384
Shompoth KHAN <dadoulan@gmail.com>
2021-10-21 09:22:15 +0200

    made call api and stored in state

--- screens/Posts.js
+++ screens/Posts.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// Librairies
+import React, { useState, useEffect } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 const Posts = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Posts</Text>
-        </View>
-    );
+    // State
+    const [postsArray, setPostsArray] = useState([]);
+
+    // UseEffect
+    useEffect(() => {
+        axios
+            .get(`https://jsonplaceholder.typicode.com/posts`)
+            .then(res => {
+                setPostsArray(res.data.splice(1, 50));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+    return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: "center", alignItems: "center" },
+    container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 10 },
 });
 
 export default Posts;
